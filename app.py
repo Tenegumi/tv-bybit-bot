c82166c5eb3d394e3a54c98a1f2787fde9f314bb
Tenegumi <78403835+Tenegumi@users.noreply.github.com>
2025-04-27 08:50:52 +0900

    Update app.py

--- app.py
+++ app.py
@@ -42,3 +42,8 @@ import os
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))
     app.run(host="0.0.0.0", port=port)
+
+# 
+@app.get("/")
+def home():
+    return "alive", 200
